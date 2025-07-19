eca5a618c23573edc81677ec139780275b09586e
ntut-xuan <t109590031@ntut.org.tw>
2025-07-19 09:01:57 +0800

    Refactor: login

--- include/auth/login.hpp
+++ include/auth/login.hpp
@@ -1,11 +1,14 @@
 #ifndef LOGIN_MENU_HPP
 #define LOGIN_MENU_HPP
 
+#include "user/user.hpp"
+#include <memory>
 #include <string>
 #include <unistd.h>
 
 class LoginPortal {
   private:
+    static std::shared_ptr<User> get_user_by_account(std::string account);
     static void authenticate(std::string name, std::string password);
 
   public:
--- src/auth/login.cpp
+++ src/auth/login.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
-#include <optional>
+#include <memory>
 #include <unistd.h>
 
 #include "auth/login.hpp"
 #include "system/student_manage/student.hpp"
 #include "system/student_manage/student_manage.hpp"
 #include "user/admin.hpp"
+#include "user/user.hpp"
 
 void LoginPortal::authenticate(std::string username, std::string password) {}
+
+std::shared_ptr<User> LoginPortal::get_user_by_account(std::string account) {
+    if (account == "admin") {
+        return std::make_shared<User>(Admin::get_instance());
+    }
+
+    optional<Student> optional_student =
+        StudentManageSystem::get_instance().get_student(account);
+
+    if (optional_student.has_value()) {
+        return std::make_shared<User>(optional_student.value());
+    } else {
+        return NULL;
+    }
+}
 
 void LoginPortal::login() {
     std::cout << "DormMS v0.1" << std::endl;
@@ -20,18 +36,9 @@ void LoginPortal::login() {
 
         char *password = getpass("Password: ");
 
-        optional<Student> optional_student =
-            StudentManageSystem::get_instance().get_student(account);
+        std::shared_ptr<User> user = get_user_by_account(account);
 
-        if (account == "admin" &&
-            Admin::get_instance().authenticate(password)) {
-            std::cout << "Login Success." << std::endl;
-            std::cout << std::endl;
-            break;
-        }
-
-        if (optional_student.has_value() &&
-            optional_student->authenticate(password)) {
+        if (user != NULL && user->authenticate(password)) {
             std::cout << "Login Success." << std::endl;
             std::cout << std::endl;
             break;
