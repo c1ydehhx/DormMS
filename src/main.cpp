a35e3fdc573bc5626f3d9376d5842741f3b45d8b
ntut-xuan <t109590031@ntut.org.tw>
2025-07-19 09:06:22 +0800

    Implement: Add comment on main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,15 @@
 #include "user/admin.hpp"
 
 int main(int argc, char *argv[]) {
+    // System Init Phase
     Configuration::get_instance().load_configuration(argv[1]);
-
     Admin::initialize_admin();
-
     StudentManageSystem::get_instance().read_students();
 
+    // Login
     LoginPortal::login();
 
+    // Enter main menu
+    // TODO: RBAC to control the menu.
     MainMenu::enter_main_menu();
 }
